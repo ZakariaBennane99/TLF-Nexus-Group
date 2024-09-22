8d80b5d073907c94b95efbd0b294392231a68ffe
Zakaria Bennane <zakariamonre@gmail.com>
2024-09-22 22:57:51 +0200

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/{repo-name}' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/{repo-name}' : '',
+    images: {
+      unoptimized: true,
+    },
+    // This is important for GitHub Pages
+    trailingSlash: true,
+  }
+  
+  module.exports = nextConfig
